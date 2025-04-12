3148c286586a554910e45658c888946e5a45394b
Nolan Law <nolan.law@yahoo.com>
2025-04-11 19:25:01 -0500

    Initial Commit

---
+++ Capstone/CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Nolan Law on 2025-04-11.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
